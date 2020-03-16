bc7c1b2b5b6e0ed4329adf673eb523a1b3eae81f
Cumhur Duzlu <cduzlu@travix.com>
2020-03-16 09:17:54 +0100

    youtube player added.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
+import { Button } from 'antd';
+import { DesktopOutlined } from '@ant-design/icons';
+import YouTube from 'react-youtube';
 import logo from './logo.svg';
 import './App.css';
+const apiUrl = 'http://localhost:3600/';
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
 
 function App() {
+
+  const [chromecastDevices, setChromecastDevices] = useState([]);
+  const [selectedDevice, setSelectedDevice] = useState({});
+  const [playingMedia, setPlayingMedia] = useState({ media: {} });
+  const [videoPlayerOpts, setVideoPlayerOpts] = useState({
+    height: '390',
+    width: '640',
+    playerVars: { // https://developers.google.com/youtube/player_parameters
+      autoplay: 1
+    }
+  });
+
+
+
+  useEffect(() => {
+    async function GetDevices() {
+      const devicesResponse = await fetch(apiUrl + 'chromecast');
+
+      let devices = await devicesResponse.json();
+      console.log(devices)
+      setChromecastDevices(devices);
+    }
+
+    GetDevices();
+
+
+
+  }, []);
+
+  useEffect(() => {
+    async function GetDevice() {
+      debugger;
+      console.log('getting device info');
+      const deviceStatusResponse = await fetch(apiUrl + 'chromecast/' + selectedDevice.name);
+  
+      const pm = await deviceStatusResponse.json();
+  
+      const start = Math.ceil(pm.currentTime);
+      videoPlayerOpts.playerVars.start = start;
+      setVideoPlayerOpts(videoPlayerOpts);
+      setPlayingMedia(pm);
+    }
+
+    GetDevice();
+    var self = this;
+    setInterval(GetDevice.bind(self), 10000);
+  }, [selectedDevice]);
+
+
+
+  function selectChromeCastDevice(device) {
+    setSelectedDevice(device);
+  }
+
+  function onVideoPlayerReady(target) {
+  }
+
+
+  const ChromeCastDevices = () => chromecastDevices.map(function (device, i) {
+    return (
+
+      <Button
+        key={device.host}
+        type="primary"
+        icon={<DesktopOutlined />}
+        onClick={() => selectChromeCastDevice(device)}
+      >
+        {device.name}
+      </Button>
+    );
+  });
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <ChromeCastDevices></ChromeCastDevices>
+
+        <YouTube
+          videoId={playingMedia.media.contentId}
+          opts={videoPlayerOpts}
+          onReady={onVideoPlayerReady}
+        />
+
       </header>
     </div>
   );
